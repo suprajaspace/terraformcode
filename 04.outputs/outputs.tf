8941db7309addc5a8f3b10e61e48fc1b5366d7b2
supraja <supra556699@gmail.com>
2024-09-23 19:39:18 +0800

    outputs concept

---
+++ 04.outputs/outputs.tf
@@ -0,0 +1,12 @@
+output "public_ip" {
+  value       = aws_instance.terraform.public_ip
+  sensitive   = false
+  description = "This is the public IP of Instance created"
+}
+output "private_ip" {
+  value       = aws_instance.terraform.private_ip
+  sensitive   = true
+  description = "This is the private IP of Instance created"
+}
+
+
